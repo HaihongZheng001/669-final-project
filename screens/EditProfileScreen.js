f275ca71e27897e08de774f1e644d11c0644bc7f
HaihongZheng001 <89872150+HaihongZheng001@users.noreply.github.com>
2023-12-02 19:06:41 -0500

    update edit profile screen

--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -1,22 +1,72 @@
 
 import { useState, useEffect } from 'react';
-import { View, Text, TextInput, StyleSheet, Alert } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Alert, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard,} from 'react-native';
 import { Button } from '@rneui/themed';
-import { useDispatch } from 'react-redux';
 import { signIn, signUp, subscribeToAuthChanges } from '../AuthManager';
 import { Header } from '../components/Header';
 import { generalStyles } from '../styles/Styles';
+import { useSelector, useDispatch } from 'react-redux';
+import { loadUsers, updateUser } from '../data/Actions';
+import { getAuth } from 'firebase/auth';
 
-
-function EditProfileScreen({navigation}) {
-  const [username, setUsername] = useState('');
+function EditProfileScreen( props ) {
+  const { navigation, route } = props;
   const [department, setDepartment] = useState('');
   const [currentMajor, setCurrentMajor] = useState('');
   const [undergraduateMajor, setUndergraduateMajor] = useState('');
 
+  const [curUserName, setCurUserName] = useState('')
+  const users = useSelector(state => state.users)
+  // console.log('users!!', users)
+  const dispatch = useDispatch();
+  const [curUser, setCurUser] = useState({})
+  const [loginUser, setLoginUser] = useState(null);
+
+
+  useEffect(()=>{
+    dispatch(loadUsers());
+  }, []);
+
+  useEffect(() => {
+    const auth = getAuth();
+    auth.onAuthStateChanged((user) => {
+      // console.log('!!auth user', user)
+      setLoginUser(user);
+    });
+  }, []);
+
+  useEffect(() => {
+    let currentUser;
+    if (loginUser) {
+      currentUser = users.find(user => user.email === loginUser.email);
+    }
+    if (currentUser) {
+      setCurUserName(currentUser.name)
+      setCurUser(prevUser => {
+        const user = users.find(user => user.email === loginUser.email && user.id);
+        setDepartment(user.department);
+        setCurrentMajor(user.currentMajor);
+        setUndergraduateMajor(user.undergraduateMajor);
+        return user;
+      })
+    }
+  }, [users])
+
   return (
-    <View style={styles.container}>
-      <Header title={'Edit Profile'}/>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+
+    <KeyboardAvoidingView 
+      behavior='padding'
+      keyboardVerticalOffset={300}
+      style={styles.container}
+    >
+      <Header title={'Edit Profile'} navigation={navigation} showBackButton={true}/>
+
+      {/* <KeyboardAvoidingView 
+        behavior='padding'
+        keyboardVerticalOffset={350}
+        style={styles.body}
+      > */}
       <View style={styles.body}>
         <View style={styles.listItemContainer}>
           <View style={styles.labelContainer}>
@@ -27,7 +77,24 @@ function EditProfileScreen({navigation}) {
               style={styles.inputBox}
               autoCapitalize='none'
               spellCheck={false}
-              value={"test1@gmail.com"}
+              value={curUserName}
+              editable={false}
+              backgroundColor={'lightgrey'}
+              color={'grey'}
+            />
+          </View>
+        </View>
+
+        <View style={styles.listItemContainer}>
+          <View style={styles.labelContainer}>
+            <Text style={styles.labelText}>User Email</Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput 
+              style={styles.inputBox}
+              autoCapitalize='none'
+              spellCheck={false}
+              value={loginUser? loginUser.email : 'placeholder'}
               editable={false}
               backgroundColor={'lightgrey'}
               color={'grey'}
@@ -42,11 +109,11 @@ function EditProfileScreen({navigation}) {
           <View style={styles.inputContainer}>
             <TextInput 
               style={styles.inputBox}
-              placeholder='enter department' 
               autoCapitalize='none'
               spellCheck={false}
               onChangeText={text=>setDepartment(text)}
               value={department}
+              // value={curUser.department&& curUser.department.length != 0 ? curUser.department:department}
             />
           </View>
         </View>
@@ -58,10 +125,10 @@ function EditProfileScreen({navigation}) {
           <View style={styles.inputContainer}>
             <TextInput 
               style={styles.inputBox}
-              placeholder='enter current major' 
               autoCapitalize='none'
               spellCheck={false}
               onChangeText={text=>setCurrentMajor(text)}
+              // value={curUser.currentMajor&& curUser.currentMajor.length != 0 ? curUser.currentMajor:currentMajor}
               value={currentMajor}
             />
           </View>
@@ -74,11 +141,11 @@ function EditProfileScreen({navigation}) {
           <View style={styles.inputContainer}>
             <TextInput 
               style={styles.inputBox}
-              placeholder='enter current major' 
               autoCapitalize='none'
               spellCheck={false}
               onChangeText={text=>setUndergraduateMajor(text)}
               value={undergraduateMajor}
+              // value={curUser.undergraduateMajor&& curUser.undergraduateMajor.length != 0 ? curUser.undergraduateMajor:undergraduateMajor}
             />
           </View>
         </View>
@@ -86,15 +153,34 @@ function EditProfileScreen({navigation}) {
         <View style={styles.buttonContainer}>
           <Button
             onPress={() =>{
-              console.log('submit profile!');
-              navigation.navigate('Home');
+              // console.log(curUser)
+              dispatch(updateUser({
+                ...curUser,
+                department: department,
+                currentMajor: currentMajor,
+                undergraduateMajor: undergraduateMajor
+              }));
+              navigation.navigate('AccountPage');
             }}
           >
-            Submit
+            Save
           </Button>  
         </View> 
+
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={() =>{
+              navigation.navigate('AccountPage');
+            }}
+          >
+            Cancel
+          </Button>  
+        </View> 
+
+      {/* </KeyboardAvoidingView> */}
       </View>
-    </View>
+    </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 }
 
