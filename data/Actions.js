d598705af46397ec7fb6582f9a5694ad675cdb16
HaihongZheng001 <89872150+HaihongZheng001@users.noreply.github.com>
2023-12-07 17:52:50 -0500

    implement add and load features

--- data/Actions.js
+++ data/Actions.js
@@ -1,9 +1,10 @@
 import { initializeApp } from 'firebase/app';
-import { addDoc, updateDoc,setDoc, doc, getFirestore, collection, onSnapshot } from 'firebase/firestore';
+import { addDoc, updateDoc,setDoc, doc, getFirestore, collection, onSnapshot, getDocs, query, where, serverTimestamp, getDoc } from 'firebase/firestore';
 // import { firebaseConfig } from '../Secrets';
-import { ADD_USER, LOAD_USERS, UPDATE_USER, ADD_REVIEW } from './Reducer';
-import { getDocs } from 'firebase/firestore';
+import { ADD_USER, LOAD_USERS, UPDATE_USER, ADD_REVIEW, LOAD_COURSES, LOAD_INSTRUCTORS, LOAD_LOGIN_USER_REVIEWS, LOAD_COURSE_REVIEWS } from './Reducer';
 import { app, db, auth } from '../firebase';
+import { Firestore, arrayUnion } from 'firebase/firestore';
+
 
 
 
@@ -58,6 +59,8 @@ import { app, db, auth } from '../firebase';
 //   }
 // }
 
+
+
 const addUser = (newUser) => {
   return async dispatch => {
     try {
@@ -77,6 +80,58 @@ const addUser = (newUser) => {
   }
 }
 
+const addReview = (newReview) => {
+  return async dispatch => {
+    try {
+      const docRef = await addDoc(collection(db, 'reviews'), newReview);
+      const reviewWithId = { ...newReview, id: docRef.id };
+      await updateDoc(doc(db, 'reviews', docRef.id), reviewWithId);
+
+      // add id to user document
+      const q = query(collection(db, 'users'), where('uid', '==', newReview.userUid));
+      const querySnapshot = await getDocs(q);
+      const userDoc = querySnapshot.docs[0];
+      const userDocRef = doc(db, 'users', userDoc.id);
+      await updateDoc(userDocRef, {
+        myReviews: arrayUnion(docRef.id) // Add the review ID to the 'reviews' array in the user document
+      });
+
+      dispatch({
+        type: ADD_REVIEW,
+        payload: {
+          review: reviewWithId
+        }
+      });
+    } catch (error) {
+      console.error("Error adding review to Firestore: ", error);
+    }
+  }
+
+}
+
+// !!add review as it's inside a subcollection of a course
+// const addReview = (newReview) => {
+//   // console.log('!!!!!!!',newReview)
+//   const courseId = newReview.courseId;
+//   // console.log('!!!', courseId)
+
+//   return async dispatch => {
+//     try {
+//       const docRef = await addDoc(collection(db, 'courses', courseId, 'reviews'), newReview);
+//       const reviewWithId = { ...newReview, id: docRef.id };
+//       await updateDoc(doc(db, 'courses',courseId,'reviews',docRef.id), reviewWithId);
+
+//       dispatch({
+//         type: ADD_REVIEW,
+//         payload: {
+//           review: reviewWithId
+//         }
+//       });
+//     } catch (error) {
+//       console.error("Error adding review to Firestore: ", error);
+//     }
+//   }
+// }
 
 
 const loadUsers = () => {
@@ -92,6 +147,128 @@ const loadUsers = () => {
       type: LOAD_USERS,
       payload: {
         newUsers: newUsers
+      }
+    });
+  }
+}
+
+const loadCourses = () => {
+  return async (dispatch) => {
+    let querySnapshot = await getDocs(collection(db, 'courses'));
+    let newCourses = querySnapshot.docs.map(docSnap => {
+      return {
+        ...docSnap.data(),
+        // key: docSnap.id
+      }
+    });
+    dispatch({
+      type: LOAD_COURSES,
+      payload: {
+        newCourses: newCourses
+      }
+    });
+  }
+}
+
+//? need to improve
+const loadLoginUserReviews = (uid) => {
+  return async (dispatch) => {
+    const q = query(collection(db, 'users'), where('uid', '==', uid));
+    const querySnapshot = await getDocs(q);
+    const userDoc = querySnapshot.docs[0];
+    const userDocRef = doc(db, 'users', userDoc.id);
+
+    const userDocSnapshot = await getDoc(userDocRef);
+    const reviewIds = userDocSnapshot.data().myReviews || [];
+    // console.log('@@@@@!!reviewIds', reviewIds)
+
+    const reviews = [];
+
+    for (const reviewId of reviewIds) {
+      const reviewDocRef = doc(db, 'reviews', reviewId); // 'reviews' is the collection name
+      const reviewDocSnapshot = await getDoc(reviewDocRef);
+      if (reviewDocSnapshot.exists()) {
+        const reviewData = reviewDocSnapshot.data();
+        reviews.push(reviewData);
+      }
+    }
+    // console.log('@@@actions', reviews)
+
+    // let querySnapshot = await getDocs(collection(db, 'reviews'));
+    // let newReviews = querySnapshot.docs.map(docSnap => {
+    //   return {
+    //     ...docSnap.data(),
+    //     // key: docSnap.id
+    //   }
+    // });
+    
+    dispatch({
+      type: LOAD_LOGIN_USER_REVIEWS,
+      payload: {
+        loginUserReviews: reviews
+      }
+    });
+  }
+}
+
+const loadCourseReviews = (courseId) => {
+  
+  return async (dispatch) => {
+    console.log("Course ID:", courseId);
+
+    const q = query(collection(db, 'reviews'), where('courseId', '==', courseId));
+    const querySnapshot = await getDocs(q);
+    const reviews = querySnapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+    
+    dispatch({
+      type: LOAD_COURSE_REVIEWS,
+      payload: {
+        courseReviews: reviews
+      }
+    });
+  }
+}
+
+
+// !!functions to load subcollection in courses
+// const loadCourses = () => {
+//   return async (dispatch) => {
+//     let querySnapshot = await getDocs(collection(db, 'courses'));
+//     let newCourses = await Promise.all(querySnapshot.docs.map(async (docSnap)=> {
+//       const courseData = docSnap.data();
+//       const reviewsSnapshot = await getDocs(collection(db, 'courses', docSnap.id, 'reviews'));
+//       const reviews = reviewsSnapshot.docs.map(doc => doc.data());
+//       return {
+//          ...courseData, reviews
+//         // ...docSnap.data(),
+//         // key: docSnap.id
+//       }
+//     }));
+//     dispatch({
+//       type: LOAD_COURSES,
+//       payload: {
+//         courses: newCourses
+//       }
+//     });
+//   }
+// }
+
+const loadInstructors = () => {
+  return async (dispatch) => {
+    let querySnapshot = await getDocs(collection(db, 'instructors'));
+    let newInstructors = querySnapshot.docs.map(docSnap => {
+      return {
+        ...docSnap.data(),
+        // key: docSnap.id
+      }
+    });
+    dispatch({
+      type: LOAD_INSTRUCTORS,
+      payload: {
+        newInstructors: newInstructors
       }
     });
   }
@@ -131,4 +308,7 @@ const updateUser = (updatedUser) => {
     }
   };
 }
-export { addUser, updateUser, loadUsers }
+
+
+
+export { addUser, updateUser, loadUsers, addReview, loadCourses, loadInstructors, loadLoginUserReviews, loadCourseReviews }
--- data/Reducer.js
+++ data/Reducer.js
@@ -1,10 +1,20 @@
 const ADD_USER = 'ADD_USER';
 const LOAD_USERS = 'LOAD_USERS';
 const UPDATE_USER = 'UPDATE_USER';
+const LOAD_COURSES = 'LOAD_COURSES';
+const LOAD_INSTRUCTORS = 'LOAD_INSTRUCTORS';
+const ADD_REVIEW = 'ADD_REVIEW';
+const LOAD_LOGIN_USER_REVIEWS = 'LOAD_LOGIN_USER_REVIEWS';
+const LOAD_COURSE_REVIEWS = 'LOAD_COURSE_REVIEWS';
 
 
 const initialState = {
   users: [],
+  courses: [],
+  instructors: [],
+  reviews: [],
+  loginUserReviews: [],
+  courseReviews: []
 }
 
 const addUser = (state, newUser) => {
@@ -24,6 +34,34 @@ const loadUsers = (state, users) => {
   }
 }
 
+const loadCourses = (state, courses) => {
+  return {
+    ...state, 
+    courses: [...courses]
+  }
+}
+
+const loadLoginUserReviews = (state, loginUserReviews) => {
+  return {
+    ...state, 
+    loginUserReviews: loginUserReviews
+  }
+}
+
+const loadCourseReviews = (state, courseReviews) => {
+  return {
+    ...state, 
+    courseReviews: courseReviews
+  }
+}
+
+const loadInstructors = (state, instructors) => {
+  return {
+    ...state, 
+    instructors: [...instructors]
+  }
+}
+
 const updateUser = (state, userId, updatedUser) => {
   let newUser = {...updatedUser};
   return {
@@ -31,6 +69,52 @@ const updateUser = (state, userId, updatedUser) => {
     users: state.users.map(user=>user.id === userId ? newUser : user)
   };
 }
+
+const addReview = (state, newReview) => {
+  let review = {
+    ...newReview,
+  }
+  return {
+    ...state, 
+    reviews: [...state.reviews, review]
+  };
+}
+
+
+
+//!!add review as subcollection of a course
+// const addReview = (state, newReview) => {
+//   // console.log('reducer!!! review', newReview)
+//   const courseId = newReview.courseId;
+//   // console.log('reducer???courseId', courseId)
+//   const courseIndex = state.courses.findIndex(course => course.id === courseId);
+//   // console.log('reducer???courseIndex', courseIndex)
+//   // console.log('reducer???state.courses=', state.courses[1].reviews)
+//   // const updatedReviews = [...state.courses[courseIndex].reviews, newReview];
+//   // const updatedReviews = [...state.courses[courseIndex].reviews, newReview];
+
+//   // const courseIndex = state.courses.findIndex(course => course.id === courseId);
+//   if (courseIndex === -1) {
+//     // Course not found, handle this case appropriately
+//     return state;
+//   }
+
+//   // Create a new review array with the new review added
+//   const updatedReviews = [...state.courses[courseIndex].reviews, newReview];
+
+//   // Create a new updated course object
+//   const updatedCourse = { ...state.courses[courseIndex], reviews: updatedReviews };
+
+//   // Create a new array of courses with the updated course
+//   const updatedCourses = [
+//     ...state.courses.slice(0, courseIndex),
+//     updatedCourse,
+//     ...state.courses.slice(courseIndex + 1),
+//   ];
+
+//   // Return the updated state
+//   return { ...state, courses: updatedCourses };
+// }
 
 const rootReducer = (state=initialState, action) => {
   switch (action.type) {
@@ -40,8 +124,18 @@ const rootReducer = (state=initialState, action) => {
       return addUser(state, action.payload.user);
     case UPDATE_USER:
       return updateUser(state, action.payload.id, action.payload.user);
+    case LOAD_COURSES:
+      return loadCourses(state, action.payload.newCourses);
+    case LOAD_INSTRUCTORS:
+      return loadInstructors(state, action.payload.newInstructors);
+    case ADD_REVIEW:
+      return addReview(state, action.payload.review);
+    case LOAD_LOGIN_USER_REVIEWS:
+      return loadLoginUserReviews(state, action.payload.loginUserReviews);
+    case LOAD_COURSE_REVIEWS:
+      return loadCourseReviews(state, action.payload.courseReviews)
     default:
       return state;
   }
 }
-export { rootReducer, ADD_USER, LOAD_USERS, UPDATE_USER };
+export { rootReducer, ADD_USER, LOAD_USERS, UPDATE_USER, LOAD_COURSES, LOAD_INSTRUCTORS, ADD_REVIEW, LOAD_LOGIN_USER_REVIEWS, LOAD_COURSE_REVIEWS };
