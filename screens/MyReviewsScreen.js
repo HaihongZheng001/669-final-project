db3e435259c7f292cd84d5b82279d59256f6b6f3
HaihongZheng001 <89872150+HaihongZheng001@users.noreply.github.com>
2023-12-12 15:10:23 -0500

    add conditional rendering for different review status

--- screens/MyReviewsScreen.js
+++ screens/MyReviewsScreen.js
@@ -1,13 +1,15 @@
 import React, {useState, useEffect} from 'react';
-import { View, Text, StyleSheet, FlatList , TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, FlatList , TouchableOpacity, Image } from 'react-native';
 import { Header } from '../components/Header';
 import { generalStyles } from '../styles/Styles';
 import { useSelector, useDispatch } from 'react-redux';
-import { loadCourses, loadLoginUserReviews } from '../data/Actions';
+import { loadCourses, loadReviews } from '../data/Actions';
 import { getAuth } from 'firebase/auth';
 import { ReviewCard } from '../components/ReviewCard';
 import { ScrollView } from 'react-native-web';
-
+import { Skeleton } from '@rneui/themed';
+import { Button } from 'react-native-paper';
+import smile from '../assets/smile.png';
 
 function MyReviewsScreen(props) {
   const { navigation, route } = props
@@ -16,31 +18,33 @@ function MyReviewsScreen(props) {
   const [loginUserReviews, setLoginUserReviews] = useState([])
   const [showAnimation, setShowAnimation] = useState(false)
 
-
-
   const dispatch = useDispatch();
 
-  const initLoginUserReviews = useSelector(state => {
-    return state.loginUserReviews
+  // const initLoginUserReviews = useSelector(state => {
+  //   return state.loginUserReviews
+  // })
+
+  const allReviews = useSelector(state => {
+    return state.reviews
   })
 
   const courses = useSelector(state => {
     return state.courses
   })
 
+  useEffect(() => {
+    dispatch(loadReviews())
+  }, [allReviews])
 
   useEffect(()=>{
-     if (loginUser) {
-      dispatch(loadLoginUserReviews(loginUser.uid))
+     if (loginUser && allReviews) {
+      const filteredReviews = allReviews.filter(review => review.userUid == loginUser.uid);
+      // console.log('hha', filteredReviews)
+      setLoginUserReviews(filteredReviews)
      } 
-  }, [loginUser]);
+  }, [loginUser, allReviews]);
 
-  useEffect(() => {
-    if (initLoginUserReviews) {
-      setLoginUserReviews(initLoginUserReviews)
-    }
-  }, [initLoginUserReviews])
- 
+
 
   useEffect(() => {
     dispatch(loadCourses())
@@ -53,12 +57,13 @@ function MyReviewsScreen(props) {
 
 
   return (
+    
     <View style={styles.container}>
       <Header title={'My Reviews'} showBackButton={true} navigation={navigation}/>
       <View style={styles.body}>
-        {/* {console.log('!!loginUserReviews', loginUserReviews.length)} */}
-
-        {loginUserReviews && loginUserReviews.length > 0 && 
+      
+        {
+          loginUserReviews && loginUserReviews.length > 0 ? 
           <FlatList
             data={loginUserReviews} // Provide the data source
             renderItem={({ item }) => (
@@ -67,7 +72,54 @@ function MyReviewsScreen(props) {
 
             )}
             keyExtractor={(item) => item.id} // Provide a unique key for each item
-          />}
+          />
+           :
+           (loginUserReviews && loginUserReviews.length === 0 ? 
+              (<View style={styles.body}>
+                  {/* <Text style={{ fontSize: 18, marginBottom: '10%', marginTop:'30%' }}>You don't have any reviews...</Text> */}
+                  <View style={{ alignItems:'center' }}>
+                    <Image source={smile} style={{ width: 300, height:300, marginBottom:'2%' }}/>
+
+                    <Button mode='contained' style={{  backgroundColor:'#5F32D1' }} onPress={() => navigation.navigate('EditReview', { screen: 'MyReviews' })}>Write a Review</Button>
+                  </View>
+                </View>)
+                
+                :
+              (<View>
+                  <Skeleton width={'100%'} height={230} backgroundColor='#EAE1FC' borderRadius={6} marginBottom={20} skeletonStyle={{ backgroundColor:'#F8F5FF' }} />
+                  <Skeleton width={'100%'} height={230} backgroundColor='#EAE1FC' borderRadius={6} marginBottom={20} skeletonStyle={{ backgroundColor:'#F8F5FF' }} />
+                  <Skeleton width={'100%'} height={230} backgroundColor='#EAE1FC' borderRadius={6} marginBottom={20} skeletonStyle={{ backgroundColor:'#F8F5FF' }} />
+                </View>) )
+           
+          // <View>
+          //   <Skeleton style={styles.stack} animation="waive" width={335} height={180} />
+          //   <Skeleton style={styles.stack} animation="pulse" width={335} height={180} />
+          //   <Skeleton style={styles.stack} animation="pulse" width={335} height={180} />
+          // </View>
+        //   <FlatList
+        //   data={loginUserReviews} // Provide the data source
+        //   renderItem={({ item }) => (
+        //       // <Text>{item.reviewContent}</Text>
+        //       <ReviewCard navigation={navigation} reviewObj={item} courseObjs={courses} showAnimation={showAnimation}/>
+
+        //   )}
+        //   keyExtractor={(item) => item.id} // Provide a unique key for each item
+        // />
+
+        }
+       
+        {/* {console.log('!!loginUserReviews', loginUserReviews.length)} */}
+
+        {/* {loginUserReviews && loginUserReviews.length > 0 && 
+          <FlatList
+            data={loginUserReviews} // Provide the data source
+            renderItem={({ item }) => (
+                // <Text>{item.reviewContent}</Text>
+                <ReviewCard navigation={navigation} reviewObj={item} courseObjs={courses} showAnimation={showAnimation}/>
+
+            )}
+            keyExtractor={(item) => item.id} // Provide a unique key for each item
+          />} */}
       </View>
     </View>
   );
@@ -75,6 +127,9 @@ function MyReviewsScreen(props) {
 
 const styles = StyleSheet.create({
   ...generalStyles,
+  stack: {
+    marginBottom: '10%',
+  }
 
 });
 
